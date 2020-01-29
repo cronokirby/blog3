74f6434a0f601ba9ee4564ab70ff53bc109e3410
Lúcás Meier <cronokirby@gmail.com>
2020-01-29 20:43:37 +0100

    Tinkering

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,7 @@
-import React from "react"
+import React from 'react';
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div className="font-serif text-6xl font-bold bg-brown-300 text-brown-900">
+    <span className="border-b-2 border-brown-900">Sharing My Ideas</span>
+  </div>
+);
