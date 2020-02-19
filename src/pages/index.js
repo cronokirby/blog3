b3496ff835cb216c28a99c8b1e107add6b30584c
Lúcás Meier <cronokirby@gmail.com>
2020-02-19 09:39:29 +0100

    Finish index page, more or less

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import { Link } from 'gatsby';
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
@@ -22,17 +23,34 @@ const Blob1 = () => (
 
 const Blob2 = () => (
   <svg
-    width="140%"
-    height="180%"
-    viewBox="0 0 450 450"
+    width="100%"
+    height="100%"
+    viewBox="0 0 433.87839 400.13974"
     className="absolute"
     preserveAspectRatio="none"
     overflow="visible"
-    xmlns="http://www.w3.org/2000/svg"
   >
-    <g transform="translate(120,110)">
+    <g transform="translate(110,200) scale(1.4, 1.1)">
       <path
-        d="M121.9,-124.6C161.9,-111.7,201.1,-77,212.7,-34.1C224.3,8.8,208.3,59.9,178.2,94.3C148.1,128.8,104,146.6,66.6,142.9C29.2,139.1,-1.5,113.7,-46,106.2C-90.4,98.7,-148.7,109.1,-181.9,87.6C-215.1,66.1,-223.3,12.6,-211.6,-34.5C-199.9,-81.6,-168.4,-122.4,-129.8,-135.6C-91.3,-148.8,-45.6,-134.4,-2.4,-131.6C40.9,-128.8,81.8,-137.5,121.9,-124.6Z"
+        d="m 140.2,-197.3 c 44,7.4 83.8,42.1 108.7,86.6 25,44.5 35,98.7 16.9,141.2 C 247.6,72.9 201.3,103.6 164,136.4 126.7,169.1 98.4,204 66.9,201 35.4,198 0.7,157 -30.3,133.9 c -31,-23.1 -58.1,-28.4 -77.5,-44.1 -19.4,-15.8 -31,-42 -39.1,-71.1 -8,-29.2 -12.4,-61.3 -11.9,-103.2 0.4,-41.9 5.8,-93.5 33.1,-108.6 27.3,-15 76.5,6.5 125.2,7.4 48.6,0.8 96.7,-19 140.7,-11.6 z"
+        fill="white"
+      />
+    </g>
+  </svg>
+);
+
+const Blob3 = () => (
+  <svg
+    width="100%"
+    height="100%"
+    viewBox="0 0 493.62344 408.27905"
+    className="absolute"
+    preserveAspectRatio="none"
+    overflow="visible"
+  >
+    <g transform="translate(320, 140) scale(1.6, 1.1)">
+      <path
+        d="m 88.9,-129.4 c 43.5,19 107.8,14.6 134.1,37.7 26.3,23 14.5,73.6 -13.8,106.7 C 180.9,48 136.2,63.6 105,80.1 73.8,96.6 56.2,114 29.1,141.3 2,168.6 -34.7,205.7 -73.6,211.9 c -38.9,6.3 -80.1,-18.3 -112.2,-52.5 -32,-34.1 -54.7,-77.8 -64.8,-125.5 -10.1,-47.6 -7.6,-99.1 22.2,-127.4 29.7,-28.3 86.6,-33.5 129.2,-52.8 42.6,-19.4 70.9,-53.1 96.4,-48.7 25.5,4.3 48.2,46.7 91.7,65.6 z"
         fill="white"
       />
     </g>
@@ -49,7 +67,7 @@ export default () => (
       ></meta>
     </Helmet>
     <nav>
-      <ul className="flex justify-around text-2xl font-bold text-white md:mx-8 py-2">
+      <ul className="flex justify-around py-2 text-2xl font-bold text-white md:mx-8">
         <li>Posts</li>
         <li>Contact</li>
         <li>Projects</li>
@@ -58,23 +76,23 @@ export default () => (
     <div className="flex flex-col items-center w-full mt-12 md:mt-24">
       <div className="relative inline-block">
         <Blob1 />
-        <h1 className="relative z-10 font-bold text-4xl sm:text-5xl md:text-8xl text-main-600">
+        <h1 className="relative z-10 text-4xl font-bold sm:text-5xl md:text-8xl text-main-600">
           Lúcás Meier
         </h1>
         <h2 className="relative z-10 text-2xl sm:text-3xl md:text-6xl text-main-600">
           Fullstack Developer
         </h2>
       </div>
-      <div className="flex flex-wrap items-center justify-center mt-32">
+      <div className="flex flex-wrap items-center justify-center mt-16 md:mt-32">
         <img
           src="/fluff/1.png"
-          className="rounded-lg w-11/12 sm:w-5/12 md:w-5/12 z-10 shadow-lg"
+          className="z-10 w-11/12 rounded-lg shadow-lg sm:w-5/12 md:w-5/12"
         ></img>
         <div className="relative inline-block h-64 mx-8">
           <Blob2 />
           <div className="relative z-10 flex flex-wrap items-center justify-center py-4">
-            <div className="relative z-10 text-main-600 mx-4">
-              <h1 className="font-bold text-2xl sm:text-3xl md:text-4xl text-main-600">
+            <div className="relative z-10 mx-4 text-main-600">
+              <h1 className="text-2xl font-bold sm:text-3xl md:text-4xl text-main-600">
                 I make websites
               </h1>
               <p className="text-sm">
@@ -84,16 +102,38 @@ export default () => (
                 </a>
                 .
               </p>
-              <p className="text-lg w-64">
+              <p className="w-64 text-lg">
                 From what you see, to what goes on behind the curtains, I like
                 making great products.
               </p>
-              <a href="foo" className="font-bold text-xl underline mt-4">
+              <Link to="/projects" className="mt-4 text-xl font-bold underline">
                 See My Work!
-              </a>
+              </Link>
             </div>
           </div>
         </div>
+      </div>
+      <div className="flex flex-wrap-reverse items-center justify-center mt-16 md:mt-32">
+        <div className="relative inline-block h-64 mx-8">
+          <Blob3 />
+          <div className="relative z-10 flex flex-wrap items-center justify-center py-4">
+            <div className="relative z-10 mx-4 text-main-600">
+              <h1 className="text-2xl font-bold sm:text-3xl md:text-4xl text-main-600">
+                I write posts
+              </h1>
+              <p className="w-64 text-lg">
+                  I write about programming, technology, and whatever else is on my mind.
+              </p>
+              <Link to="/posts" className="mt-4 text-xl font-bold underline">
+                See My Posts!
+              </Link>
+            </div>
+          </div>
+        </div>
+        <img
+          src="/fluff/2.png"
+          className="z-10 w-11/12 rounded-lg shadow-lg sm:w-5/12 md:w-5/12"
+        ></img>
       </div>
     </div>
   </>
