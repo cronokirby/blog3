b89d31b41e079af9d6d0c91391dbfc880cb674ef
Lúcás Meier <cronokirby@gmail.com>
2020-04-14 10:13:40 +0200

    More style updates

--- src/templates/post.js
+++ src/templates/post.js
@@ -13,11 +13,17 @@ export default function Template({ data }) {
         <meta name="description" content={frontmatter.description} />
       </Helmet>
       <Layout>
-        <div className="object-contain px-2 py-2 mx-auto my-4 rounded shadow-md text-nord6 bg-nord0 sm:w-5/6 lg:w-6/12">
-          <img src={frontmatter.image} alt="" className="mx-auto h-128"></img>
+        <div className="w-11/12 px-2 py-2 mx-auto my-4 rounded shadow-md text-nord6 bg-nord0 sm:w-5/6 lg:w-6/12">
+          <img
+            src={frontmatter.image}
+            alt=""
+            className="object-contain mx-auto md:object-cover h-128"
+          ></img>
           <div className="px-4 py-4 sm:px-8">
             <h2 className="mt-2 text-xl text-brown-800">{frontmatter.date}</h2>
-            <h1 className="mb-4 text-6xl font-bold">{frontmatter.title}</h1>
+            <h1 className="mb-4 text-3xl font-bold md:text-6xl">
+              {frontmatter.title}
+            </h1>
             <div
               className="blog-post-content"
               dangerouslySetInnerHTML={{ __html: html }}
